4f2d6b0da2d3c7fcc949be6c6a8f8655db732a4a
srikanth gurram <2gurram@gmail.com>
2019-07-07 08:58:22 +0530

    Fix for '@angular/http' module not found error in latest angular (7+)

--- src/auto-complete.service.ts
+++ src/auto-complete.service.ts
@@ -1,13 +1,13 @@
 ﻿import { Injectable, PLATFORM_ID, Inject } from '@angular/core';
 import { isPlatformBrowser, isPlatformServer } from '@angular/common';
 import { GlobalRef } from './windowRef.service';
-import { Http } from '@angular/http';
+import { HttpClient } from '@angular/common/http';
 import { LocalStorageService } from './storage.service';
 import 'rxjs/Rx';
 
 @Injectable()
 export class AutoCompleteSearchService {
-  constructor(private _http: Http, @Inject(PLATFORM_ID) private platformId: Object,
+  constructor(private _http: HttpClient, @Inject(PLATFORM_ID) private platformId: Object,
   private _global: GlobalRef, private _localStorageService: LocalStorageService) {
 
   }
